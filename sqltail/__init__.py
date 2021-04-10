9d418ddf739845f490d9c109832748dec98bf3ed
Matt Krueger <mkrueger@rstms.net>
2021-04-10 17:37:05 -0500

    sync

--- sqltail/__init__.py
+++ sqltail/__init__.py
@@ -3,5 +3,5 @@
 from sqltail.monitor import SQLTail
 from sqltail.db import Database, DatabaseException, DatabaseConnectionFailed, DatabaseNotFound
 
-__version__='0.0.5'
+__version__='1.0.0'
 __license__='Copyright 2021 Reliance Systems, Inc. - MIT license - https://github.com/rstms/sqltail'
