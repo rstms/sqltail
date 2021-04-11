544454e4fd361d04adb7a2494db4835ae875bb39
Matt Krueger <mkrueger@rstms.net>
2021-04-10 21:00:23 -0500

    Bump version: 0.0.8 → 0.0.9

--- sqltail/__init__.py
+++ sqltail/__init__.py
@@ -3,5 +3,5 @@
 from sqltail.monitor import SQLTail
 from sqltail.db import Database, DatabaseException, DatabaseConnectionFailed, DatabaseNotFound
 
-__version__='0.0.8'
+__version__='0.0.9'
 __license__='Copyright 2021 Reliance Systems, Inc. - MIT license - https://github.com/rstms/sqltail'
